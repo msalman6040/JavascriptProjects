57a57880f7a6332a2958d1fbddae6c543c0305cb
Muhammad Salman <71087298+msalman6040@users.noreply.github.com>
2020-11-02 13:18:18 -0500

    Create weatherProject.js

---
+++ weatherProject.js
@@ -0,0 +1,11 @@
+// declares constant variable with number value 293
+const kelvin = 0;
+// celsius value of kelvin
+let celsius = kelvin -273;
+// fahrenheit value of celsius
+let fahrenheit = celsius *(9/5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+console.log(`the temperature is ${fahrenheit} degrees fahrenheit.`);
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`the temperature is ${newton} degrees newton.`)
